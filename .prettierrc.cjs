dc6b2dc977510d6ce2c7474565e437ee283dbe44
Miguel Velasquez <veruiz.miguel@gmail.com>
2023-08-12 10:02:25 -0500

    configs

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+/** @typedef  {import("prettier").Config} */
+const config = {
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  trailingComma: 'es5',
+  bracketSameLine: true,
+  printWidth: 80,
+};
+
+module.exports = config;
