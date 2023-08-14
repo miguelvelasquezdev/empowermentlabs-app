6d90d86238e7e3ed909e9b2201851ef8720677b2
Miguel Velasquez <veruiz.miguel@gmail.com>
2023-08-14 15:32:12 -0500

    form control added

--- src/app/modules/detail/detail.module.ts
+++ src/app/modules/detail/detail.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DetailRoutingModule } from './detail-routing.module';
 import { DetailComponent } from './detail.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    DetailComponent
-  ],
-  imports: [
-    CommonModule,
-    DetailRoutingModule
-  ]
+  declarations: [DetailComponent],
+  imports: [CommonModule, DetailRoutingModule, ReactiveFormsModule],
 })
-export class DetailModule { }
+export class DetailModule {}
